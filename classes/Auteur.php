db5d68849531aa2b42ca58581caa196d2b0f408c
John Doe11 <sataevmagomed.eu@gmail.com>
2024-02-06 11:31:42 +0100

    Exo avec les ajout des livre pour un auteur automatiquement

--- classes/Auteur.php
+++ classes/Auteur.php
@@ -3,12 +3,33 @@
 class Auteur {
     protected string $nom;
     protected string $prenom;
+    private array $livres;
 
     public function __construct(string $nom, string $prenom)
     {
         $this->nom = $nom;
         $this->prenom = $prenom;
+        $this->livres = [];
     }
+    //Methode toString
+    public function __toString()
+    {
+        return $this->nom." ".$this->prenom;
+    }
+
+    //Ajouter les livres
+    public function ajouterLivre(Livre $livre){
+        $this->livres[] = $livre;
+    }
+    //Afficher info
+    public function bibliograpfie(){
+        echo "Livres de ". $this . "<br>";
+        foreach ($this->livres as $livre) {
+            echo $livre;
+        }
+
+    }
+
 
     
     public function getNom()
@@ -32,8 +53,4 @@ class Auteur {
         return $this;
     }
 
-    public function __toString()
-    {
-        return $this->nom." ".$this->prenom;
-    }
 }
--- classes/Livre.php
+++ classes/Livre.php
@@ -3,18 +3,30 @@
 class Livre {
     private string $titre; 
     private int $nbPages;
-    private DateTime $anneeParution;
-    private int $prix;
+    private DateTime $dateParution;
+    private float $prix;
     private Auteur $auteur;
 
-    public function __construct(string $titre, int $nbPages, string $anneeParution, int $prix, Auteur $auteur)
+    public function __construct(string $titre, int $nbPages, string $dateParution, float $prix, Auteur $auteur)
     {
         $this->titre = $titre;
         $this->nbPages = $nbPages;
-        $this->anneeParution = new DateTime($anneeParution);
+        $this->dateParution = new DateTime($dateParution);
         $this->prix = $prix;
         $this->auteur = $auteur;
+
+        $this->auteur->ajouterLivre($this);
     }
+
+    public function __toString()
+    {
+        return $this->titre."(".$this->dateParution->format('Y').")  : {$this->nbPages} pages / {$this->prix} €<br>";
+    }
+
+    // public function bibliograpfie(){
+    //     echo $this->auteur;
+    //     echo "{$this} : {$this->nbPages} pages / {$this->prix} €<br>";
+    // }
 
     
 
@@ -40,13 +52,13 @@ class Livre {
         return $this;
     }
    
-    public function getAnneeParution()
+    public function getDateParution()
     {
-    return $this->anneeParution;
+    return $this->dateParution;
     }
-    public function setAnneeParution($anneeParution)
+    public function setDateParution($dateParution)
     {
-    $this->anneeParution = $anneeParution;
+    $this->dateParution = $dateParution;
 
     return $this;
     }
@@ -70,16 +82,6 @@ class Livre {
 
         return $this;
     }
-
-    public function __toString()
-    {
-        return $this->titre."(".$this->anneeParution->format('Y').")";
-    }
-
-    public function bibliograpfie(){
-        echo "{$this} : {$this->nbPages} pages / {$this->prix} €<br>";
-    }
-    
-    
+        
 }
 
